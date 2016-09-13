ff6e33219664c61f415c1a019a2a3b815bfcbcb2
Ben Sterrett <raynjamin@gmail.com>
2016-09-13 12:18:32 -0400

    Adding Paging.

--- Purchases/src/main/java/com/theironyard/repositories/PurchaseRepository.java
+++ Purchases/src/main/java/com/theironyard/repositories/PurchaseRepository.java
@@ -1,11 +1,10 @@
 package com.theironyard.repositories;
 
 import com.theironyard.entities.Purchase;
-import org.springframework.data.repository.CrudRepository;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.data.repository.PagingAndSortingRepository;
 
-import java.util.List;
-
-public interface PurchaseRepository extends CrudRepository<Purchase, Integer> {
-
-    List<Purchase> findByCategory(String category);
+public interface PurchaseRepository extends PagingAndSortingRepository<Purchase, Integer> {
+    Page<Purchase> findByCategory(Pageable pageable, String category);
 }
